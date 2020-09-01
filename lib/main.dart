94f17ed9acac1f7aeefdb47f5c4c07a4f0e66174
Hector <hveitia86@gmail.com>
2020-09-01 17:17:19 -0500

    Restart app on resume

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:heraldo_cuba_web/src/theme/theme.dart';
@@ -31,52 +29,65 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   WebViewController _controller;
   String url = '';
+  String activeCategory = '';
   final categories = [
     MyCategory(
-        FontAwesomeIcons.newspaper, 'home', 'https://heraldodecuba.com/'),
+        FontAwesomeIcons.newspaper, 'inicio', 'https://heraldodecuba.com/'),
     MyCategory(FontAwesomeIcons.heart, 'cuba',
         'https://heraldodecuba.com/category/cuba/'),
     MyCategory(FontAwesomeIcons.flagUsa, 'usa',
         'https://heraldodecuba.com/category/usa/'),
     MyCategory(FontAwesomeIcons.globe, 'internacionales',
         'https://heraldodecuba.com/category/internacionales/'),
-    MyCategory(FontAwesomeIcons.globe, 'contáctenos',
+    MyCategory(FontAwesomeIcons.envelope, 'contáctenos',
         'https://heraldodecuba.com/contactenos/'),
+    MyCategory(FontAwesomeIcons.moneyCheck, 'donaciones',
+        'https://heraldodecuba.com/contribucion-con-el-heraldo-de-cuba/'),
   ];
 
   @override
   void initState() {
     super.initState();
     setState(() {
+      this.activeCategory = categories[0].name;
       this.url = categories[0].url;
     });
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.paused) {}
+    if (state == AppLifecycleState.resumed) {
+      RestartWidget.restartApp(context);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     final _screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _getContainerList(),
-            Container(
-                height: _screenSize.height * 0.78,
-                child: WebView(
-                  initialUrl: this.url,
-                  javaScriptMode: JavaScriptMode.unrestricted,
-                  onWebViewCreated: (WebViewController c) {
-                    _controller = c;
-                  },
-                )),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _getContainerList(),
+              Container(
+                  height: _screenSize.height * 0.85,
+                  child: WebView(
+                    initialUrl: this.url,
+                    javaScriptMode: JavaScriptMode.unrestricted,
+                    onWebViewCreated: (WebViewController c) {
+                      _controller = c;
+                    },
+                  )),
+            ],
+          ),
         ),
       ),
     );
@@ -95,14 +106,56 @@ class _MyHomePageState extends State<MyHomePage> {
             onTap: () async {
               this.url = categories[index].url;
               await _controller.loadUrl(this.url);
+              this.activeCategory = categories[index].name;
+              setState(() {});
             },
             child: Padding(
               padding: EdgeInsets.all(8),
-              child: _CategoyButton(categories[index]),
+              child: _getCategoryButton(categories[index]),
             ),
           );
         },
       ),
+    );
+  }
+
+  _getCategoryButton(MyCategory myCategory) {
+    final button = Container(
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: this.activeCategory == myCategory.name
+                ? myTheme.accentColor
+                : Colors.white),
+        child: Icon(
+          myCategory.icon,
+          color: this.activeCategory == myCategory.name
+              ? Colors.white
+              : Colors.black54,
+        ),
+      ),
+    );
+
+    final text = Text(
+      '${myCategory.name[0].toUpperCase()}${myCategory.name.substring(1)}',
+      style: TextStyle(
+          fontSize: 12,
+          color: this.activeCategory == myCategory.name
+              ? myTheme.accentColor
+              : Colors.white),
+    );
+
+    return Column(
+      children: <Widget>[
+        button,
+        SizedBox(
+          height: 5,
+        ),
+        text
+      ],
     );
   }
 }
@@ -115,42 +168,33 @@ class MyCategory {
   MyCategory(this.icon, this.name, this.url);
 }
 
-class _CategoyButton extends StatelessWidget {
-  final MyCategory myCategory;
+class RestartWidget extends StatefulWidget {
+  RestartWidget({this.child});
 
-  const _CategoyButton(this.myCategory);
+  final Widget child;
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_RestartWidgetState>().restartApp();
+  }
+
+  @override
+  _RestartWidgetState createState() => _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = UniqueKey();
+
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final button = Container(
-      child: Container(
-        width: 40,
-        height: 40,
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: true ? myTheme.accentColor : Colors.white),
-        child: Icon(
-          myCategory.icon,
-          color: true ? Colors.white : Colors.black54,
-        ),
-      ),
-    );
-
-    final text = Text(
-      '${this.myCategory.name[0].toUpperCase()}${this.myCategory.name.substring(1)}',
-      style: TextStyle(
-          fontSize: 12, color: true ? myTheme.accentColor : Colors.white),
-    );
-
-    return Column(
-      children: <Widget>[
-        button,
-        SizedBox(
-          height: 5,
-        ),
-        text
-      ],
+    return KeyedSubtree(
+      key: key,
+      child: widget.child,
     );
   }
 }
